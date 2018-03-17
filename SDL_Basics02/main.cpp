673f5757509f863e9ca3a13eb8fe0b54af14cd3e
Mark Wooding <marco_polo12000@yahoo.com>
2018-03-16 22:38:42 -0700

    Initial Commit

---
+++ SDL_Basics02/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDL_Basics02
+//
+//  Created by Developer on 3/16/18.
+//  Copyright © 2018 Developer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
